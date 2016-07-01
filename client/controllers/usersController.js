ec245bf8d70dca929f142df8d4763beca461b45e
Jeff Hedfors <git.j@hedfors.net>
2016-06-30 18:21:29 -0700

    fixed login validation

--- client/controllers/usersController.js
+++ client/controllers/usersController.js
@@ -1,12 +1,19 @@
 myApp.controller('usersController',function(userFactory,$location){
   var self = this
   self.newUser = {}
+  self.errors = {}
   self.login = function(){
     var self = this
-    userFactory.login(self.name, function(response_from_factory){
-      console.log('response_from_factory', response_from_factory);
-      $location.url('/bids')
-    })
+    if (!this.name) {
+      self.errors = {missing:true}
+    }
+    else {
+      userFactory.login(self.name, function(response_from_factory){
+        console.log('response_from_factory', response_from_factory);
+        self.error = {}
+        $location.url('/bids')
+      })
+    }
   }
   self.logout = function(){
     userFactory.logout(function(){
